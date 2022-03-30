4cfeb0d733f162284e322b3af9039d25225a56d5
pichao chang <31371988+Pichaochang@users.noreply.github.com>
2022-03-31 00:43:00 +0800

    feat：项目部署配置前缀

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/blog',
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
     return 'my-build-id'
